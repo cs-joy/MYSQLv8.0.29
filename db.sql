5478c0423d4589eb1261d8655b715ed1ba4993cb
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-06-02 06:07:27 +0600

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,15 @@
+--Create Table--
+CREATE TABLE sql (
+    id int,
+    name varchar(45),
+    age int,
+    email varchar(45)
+);
+
+--Insert data into table
+
+INSERT INTO sql(id, name, age, email) VALUES (
+    1, 'kh saimon', 22, 'khsaimon@gmail.com',
+    2, 'sk sabbir', 23, 'sabbir@gmail.com'
+);
+
