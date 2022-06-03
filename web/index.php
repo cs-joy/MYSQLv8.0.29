2539fd4f6041b8d4125329bf05b3d8f4f4aa3eb8
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-06-03 15:45:16 +0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -64,7 +64,18 @@ if (isset($_POST['create'])) {
         <hr />
         <hr />
         <div>
-            
+            <?php
+              $sql = "SELECT position FROM employeer";
+              $execution = mysqli_query($connection, $sql);
+              if($execution){
+                  while($row = mysqli_fetch_assoc($execution)){
+                    $n = $row['position'];
+
+                    echo $n ."\n";
+                  }
+                  
+              }
+            ?>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
